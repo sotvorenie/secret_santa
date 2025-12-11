e86cf9c429320e1aa4a02dd36f840d25fcb04202
vitaliy abrosimov <vitalikabrosimov00@gmail.com>
2025-12-11 11:20:08 +0300

    added transition for lights-btn

--- src/components/ui/Lights.vue
+++ src/components/ui/Lights.vue
@@ -1,15 +1,22 @@
 <script setup>
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 
 import light1 from "../../assets/images/гирлянда 2.gif"
 import light2 from "../../assets/images/гирлянда.webp"
 
 const isActive = ref(false)
 
+const btnVisible = ref(false)
+
 const handleButton = () => {
   isActive.value = !isActive.value
 }
 
+onMounted(() => {
+  setTimeout(() => {
+    btnVisible.value = true
+  }, 100)
+})
 </script>
 
 <template>
@@ -19,13 +26,16 @@ const handleButton = () => {
 
     <img class="lights__two position-absolute" :src="light2" height="300" width="1366" alt=""/>
 
-    <button class="lights__btn button rounded position-absolute"
-            :class="{'is-active': isActive}"
-            type="button"
-            @click="handleButton"
-    >
-      {{isActive ? 'on' : 'off'}}
-    </button>
+    <Transition name="fade-scale">
+      <button class="lights__btn button rounded position-absolute"
+              :class="{'is-active': isActive}"
+              type="button"
+              @click="handleButton"
+              v-if="btnVisible"
+      >
+        {{isActive ? 'on' : 'off'}}
+      </button>
+    </Transition>
   </div>
 
 </template>
